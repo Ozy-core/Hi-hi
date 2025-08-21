ad81548e2702ee166be0916441c35a44e2c43f5d
Osier Nono <osiernono@gmail.com>
2025-08-21 10:41:42 -0500

    Restaring

--- Hi==hi.cpp
+++ Hi==hi.cpp
@@ -2,28 +2,9 @@
 
 using namespace std;
 
-bool strcmp(string str1, string str2)
-{
-    bool equal;
-    if (str1.length()==str2.length()){
-        for (int i; i<=str1.length()-1; i++)
-        {
-            if(str1[i]== str2[i])
-            {
-                equal= true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-}
+
 
 int main()
 {
-    bool a = strcmp("mymom","MyMom");
-
-    cout << a;
-    return 0;
+   
 }
