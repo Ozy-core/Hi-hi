b800880f294727c976e0ff5224ea081e121bb754
Osier Nono <osiernono@gmail.com>
2025-08-21 13:05:41 -0500

    changed my code for funtion

--- Hihi.cpp
+++ Hihi.cpp
@@ -4,22 +4,25 @@ using namespace std;
 
 int strcmp_case_insensitive(string one, string two)
 {
+    int charOne= 0;
+    int charTwo= 0;
     for (int i=0; i<one.length(); i++)
     {
-        int charOne= static_cast<int>(one[i]);
-        int charTwo= static_cast<int>(two[i]);
-        if(charOne > charTwo)
-        {
-            return 1;
+        charOne+= static_cast<int>(one[i]);
+        charTwo+= static_cast<int>(two[i]);
+    }
+
+    if(charOne > charTwo)
+    {
+        return 1;
         }
-        else if(charOne < charTwo)
-        {
-            return -1;
-        }
-        else if(charOne == charTwo)
-        {
-            return 0;
-        }
+    else if(charOne < charTwo)
+    {
+        return -1;
+    }
+    else if(charOne == charTwo)
+    {
+        return 0;
     }
     return -1;
 }
@@ -27,9 +30,9 @@ int strcmp_case_insensitive(string one, string two)
 int main()
 {
 
-    cout << strcmp_case_insensitive("2","50 \n");
-    cout << strcmp_case_insensitive("2","10 \n");
-    cout << strcmp_case_insensitive("2 some string","50 some string \n");
+    cout << strcmp_case_insensitive("2","50 ")<< endl;
+    cout << strcmp_case_insensitive("2","10 ") << endl;
+    cout << strcmp_case_insensitive("2 some string","50 some string ") << endl;
 
     assert(strcmp_case_insensitive("10","5"));
     assert(strcmp_case_insensitive("600","70"));
